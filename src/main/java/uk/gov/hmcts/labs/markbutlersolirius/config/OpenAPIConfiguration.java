36621d25a098f3498bbe784e1bd0fdcfa2f09853
Scaffolder <scaffolder@backstage.io>
2022-05-12 10:05:09 +0000

    Initial commit

---
+++ src/main/java/uk/gov/hmcts/labs/markbutlersolirius/config/OpenAPIConfiguration.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.labs.markbutlersolirius.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfiguration {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .info(new Info().title("labs-mark-butler-solirius")
+                      .description("labs-mark-butler-solirius")
+                      .version("v0.0.1")
+                      .license(new License().name("MIT").url("https://opensource.org/licenses/MIT")))
+            .externalDocs(new ExternalDocumentation()
+                              .description("README")
+                              .url("https://github.com/hmcts/labs-mark-butler-solirius"));
+    }
+
+}
---
+++ src/smokeTest/SmokeTestConfiguration.java
@@ -0,0 +1,10 @@
+package uk.gov.hmcts.labs.markbutlersolirius.smoketests;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+@ComponentScan("uk.gov.hmcts.labs-mark-butler-solirius.smoketests")
+@PropertySource("application.properties")
+public class SmokeTestConfiguration {
+  //TODO: implement smoke tests here
+}
